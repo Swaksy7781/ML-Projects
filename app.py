91d55f60ab19488c7c2592e1e94a5c40beb70755
Swaksy7781 <saurabhwakaseseb159@gmail.com>
2024-07-22 21:16:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
+import os
 
 app = Flask(__name__)
 
-#load the model
+# Load the model
 regmodel = pickle.load(open("regmodel.pkl", "rb"))
 scalar = pickle.load(open("scaling.pkl", "rb"))
 
@@ -13,27 +13,24 @@ scalar = pickle.load(open("scaling.pkl", "rb"))
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api', methods = ["POST"])
-
+@app.route('/predict_api', methods=["POST"])
 def predict_api():
     data = request.json["data"]
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
-@app.route("/predict", methods = ['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     data = [float(x) for x in request.form.values()]
     final_input = scalar.transform(np.array(data).reshape(1, -1))
     print(final_input)
     output = regmodel.predict(final_input)[0]
-    return render_template("home.html", prediction_text = "The house price prediction is {}".format(output))
+    return render_template("home.html", prediction_text="The house price prediction is {}".format(output))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=port)
-
-    
